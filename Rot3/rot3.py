27cb7160c677f47fae55553c2cf20cb9dfa6ddaa
dalton <daltonchivambo@gmail.com>
2022-10-19 17:02:34 +0200

    optimizing the code

--- Rot3/rot3.py
+++ Rot3/rot3.py
@@ -1,22 +1,27 @@
 import sys
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-rot = 3
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+ROT = 3
+
 
 def cipher(message, dir):
     m = ''
     for c in message:
-        if c in alphabet:
-            c_index = alphabet.index(c)
-            m += alphabet[(c_index + (dir * rot)) % len(alphabet)]
+        if c in ALPHABET:
+            c_index = ALPHABET.index(c)
+            m += ALPHABET[(c_index + (dir * ROT)) % len(ALPHABET)]
         else:
             m += c
     return m.upper()
 
+
 def encrypt(message):
     return cipher(message, 1)
+
+
 def decrypt(message):
     return cipher(message, -1)
+
 
 def main():
     command = sys.argv[1].lower()
