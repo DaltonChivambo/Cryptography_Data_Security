9f921c97ad0c8c48882fa6555d830c424fbb6f20
dalton <daltonchivambo@gmail.com>
2022-11-03 17:30:26 +0200

    Add implementation of the vigenere cypher

--- Vigenere/vigenere_encrypt.py
+++ Vigenere/vigenere_encrypt.py
@@ -1,21 +1,44 @@
 import string
 
-plaintext = "we are discovered save your self"
-key = "deceptive"
-cipher = ""
-
-index = 10
-
-for c in plaintext:
-    if c in string.ascii_lowercase:
-        offset = ord(key[index]) - ord('a')
-
-        ord(c) - ord('a') + offset
-        index = (index + 1) % len(key)
-    else:
-        # add if is not a letter
-        cipher = cipher + c
+main = string.ascii_lowercase
 
 
+def conversion(plain_text, key):
+    index = 0
+    cipher_text = ""
+
+    # convert into lower case
+    plain_text = plain_text.lower()
+    key = key.lower()
+
+    for c in plain_text:
+        if c in main:
+            # to get the number corresponding to the alphabet for key
+            alpha_key = ord(key[index]) - ord('a')
+
+            # to get the number corresponding to the alphabet for plaintext
+            alpha_text = ord(c) - ord('a')
+
+            # implementing algo logic here
+            encrypt_num = (alpha_text + alpha_key) % 26     # to get alphabet encrypted
+            convert_unicode = encrypt_num + ord('a')    # to convert for unicode
+            convert_to_string = chr(convert_unicode)
+            encrypt = convert_to_string
+
+            # adding into cipher text to get the encrypted message
+            cipher_text += encrypt
+
+            # for cyclic rotation in generating key from keyword
+            index = (index + 1) % len(key)
+        else:
+            cipher_text += c
+
+    print("plain text: ", plain_text)
+    print("cipher text: ", cipher_text)
 
 
+plain_text = input("Enter the message: ")
+key = input("Enter the key: ")
+
+# calling function
+conversion(plain_text, key)
