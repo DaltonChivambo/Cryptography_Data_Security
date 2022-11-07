863a4e84b97942bc57558384fe282bed1b06c1da
dalton <daltonchivambo@gmail.com>
2022-11-07 17:38:53 +0200

    Implementing vigenere_decrypt.py algorithm

---
+++ Vigenere/vigenere_decrypt.py
@@ -0,0 +1,38 @@
+import string
+
+main = string.ascii_lowercase
+
+
+def conversion(cipher_text, key):
+    index = 0
+    plain_text = ""
+
+    # convert into lower case
+    cipher_text = cipher_text.lower()
+    key = key.lower()
+
+    for c in cipher_text:
+        if c in main:
+            # to get the number corresponding to the alphabet
+            off = ord(key[index]) - ord('a')
+
+            positive_off = 26 - off
+            decrypt = chr((ord(c) - ord('a') + positive_off) % 26 + ord('a'))
+
+            # adding into plain text to get the decrypted messag
+            plain_text += decrypt
+
+            # for cyclic rotation in generating key from keyword
+            index = (index + 1) % len(key)
+        else:
+            plain_text += c
+
+    print("cipher text: ", cipher_text)
+    print("plain text (message): ", plain_text)
+
+
+cipher_text = input("Enter the message to be decrypted: ")
+key = input("Enter the key for decryption: ")
+
+# calling function
+conversion(cipher_text, key)
